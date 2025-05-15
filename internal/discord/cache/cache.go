dad1a17276e00d27b66492279d77bf18525a72db
Marwane Souiri <marwane.souiri.0@gmail.com>
2025-05-15 13:24:26 +0100

    refactor(discord/cache): add guildKeys map to retrieve all guilds and flush support - Add sync.Map guildKeys to track cached guilds - Add GetGuilds to retrieve all guilds from cache - Add FlushGuilds to ensure Set operations are committed - Update SetGuild and SetGuilds to track guildKeys - Edit SetGuild method to take only the guild object

--- internal/discord/cache/cache.go
+++ internal/discord/cache/cache.go
@@ -1,17 +1,27 @@
 package cache
 
 import (
+	"sync"
+
 	"github.com/dgraph-io/ristretto/v2"
 	"github.com/marouane-souiri/vocalize/internal/discord/models"
 )
 
 type DiscordCacheManager interface {
+	Close()
+
+	// guild cache crud funcs
+	SetGuild(guild *models.Guild) bool
+	SetGuilds(guilds []*models.Guild) bool
+	FlushGuilds()
+	DelGuild(ID string)
 	GetGuild(ID string) (*models.Guild, bool)
-	SetGuild(ID string, guild *models.Guild) bool
+	GetGuilds() []*models.Guild
 }
 
 type DiscordCacheManagerImpl struct {
 	guildsCache *ristretto.Cache[string, *models.Guild]
+	guildKeys   sync.Map
 }
 
 func NewDiscordCacheManager() (DiscordCacheManager, error) {
@@ -30,10 +40,62 @@ func NewDiscordCacheManager() (DiscordCacheManager, error) {
 	}, nil
 }
 
+func (c *DiscordCacheManagerImpl) Close() {
+	c.guildsCache.Close()
+}
+
+func (c *DiscordCacheManagerImpl) SetGuild(guild *models.Guild) bool {
+	if c.guildsCache.Set(guild.ID, guild, 1) {
+		c.guildKeys.Store(guild.ID, struct{}{})
+		return true
+	}
+	return false
+}
+
+func (c *DiscordCacheManagerImpl) SetGuilds(guilds []*models.Guild) bool {
+	batchSize := 60
+	batchCount := 0
+
+	for _, guild := range guilds {
+		if !c.SetGuild(guild) {
+			return false
+		}
+		batchCount++
+
+		if batchCount >= batchSize {
+			c.FlushGuilds()
+			batchCount = 0
+		}
+	}
+
+	if batchCount > 0 {
+		c.FlushGuilds()
+	}
+
+	return true
+}
+
+func (c *DiscordCacheManagerImpl) FlushGuilds() {
+	c.guildsCache.Wait()
+}
+
+func (c *DiscordCacheManagerImpl) DelGuild(ID string) {
+	c.guildsCache.Del(ID)
+	c.guildKeys.Delete(ID)
+}
+
 func (c *DiscordCacheManagerImpl) GetGuild(ID string) (*models.Guild, bool) {
 	return c.guildsCache.Get(ID)
 }
 
-func (c *DiscordCacheManagerImpl) SetGuild(ID string, guild *models.Guild) bool {
-	return c.guildsCache.Set(ID, guild, 1)
+func (c *DiscordCacheManagerImpl) GetGuilds() []*models.Guild {
+	var guilds []*models.Guild
+	c.guildKeys.Range(func(key, _ any) bool {
+		id := key.(string)
+		if val, found := c.guildsCache.Get(id); found && val != nil {
+			guilds = append(guilds, val)
+		}
+		return true
+	})
+	return guilds
 }
