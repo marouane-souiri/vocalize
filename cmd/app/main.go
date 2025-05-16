722dd5a7a48efa8f0e91bb9a320d4d84f7ee1bbf
Marwane Souiri <marwane.souiri.0@gmail.com>
2025-05-16 21:00:21 +0100

    feat(app): create commandsManager and register ping commands and messageCreate handler

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"log"
 
+	"github.com/marouane-souiri/vocalize/internal/application/commands/commands"
+	"github.com/marouane-souiri/vocalize/internal/application/commands/commandsmanager"
 	"github.com/marouane-souiri/vocalize/internal/application/handlers"
 	"github.com/marouane-souiri/vocalize/internal/config"
 	"github.com/marouane-souiri/vocalize/internal/discord/cache"
@@ -24,20 +26,24 @@ func main() {
 
 	discordCacheManager := cache.NewDiscordCacheManager()
 
-	clientOptions := &client.CLientOptions{
-		Token: config.Conf.Discord.Token,
-		Ws:    websocketManager,
-		Wp:    workerpoolManager,
-		Cm:    discordCacheManager,
-	}
-
-	client, err := client.NewClient(clientOptions)
+	client, err := client.NewClient(&client.CLientOptions{
+		Token:   config.Conf.Discord.Token,
+		Intents: client.Intents_GUILDS | client.Intents_GUILD_MESSAGES | client.Intents_MESSAGE_CONTENT,
+		Ws:      websocketManager,
+		Wp:      workerpoolManager,
+		Cm:      discordCacheManager,
+	})
 	if err != nil {
 		log.Fatalf("Failed to create discord client: %v", err)
 	}
 
+	commandsManager := commandsmanager.NewCommandsManager()
+
+	commandsManager.AddCommand(commands.NewPingCommand())
+
 	client.On("GUILD_CREATE", handlers.GuildCreateHandler(client))
 	client.On("GUILD_DELETE", handlers.GuildDeleteHandler(client))
+	client.On("MESSAGE_CREATE", handlers.MessageCreateHandler(client, commandsManager))
 
 	if err := client.Start(); err != nil {
 		log.Fatalf("Failed to start discord client: %v", err)
