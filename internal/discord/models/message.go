37bb547b6af1ae8cd7635ad61aa9e45002256345
Marwane Souiri <marwane.souiri.0@gmail.com>
2025-05-18 20:47:26 +0100

    feat: Add Discord "message send" models

--- internal/discord/models/message.go
+++ internal/discord/models/message.go
@@ -17,3 +17,49 @@ type Message struct {
 	EditedTimestamp *time.Time  `json:"edited_timestamp"`
 	Type            MessageType `json:"type"`
 }
+
+type EmbedAuthor struct {
+	Name string `json:"name"`
+	Url  string `json:"url,omitempty"`
+}
+
+type EmbedFooter struct {
+	Text string `json:"text"`
+	Url  string `json:"url,omitempty"`
+}
+
+type EmbedImage struct {
+	Url    string `json:"url"`
+	Height int    `json:"height,omitempty"`
+	Width  int    `json:"widthi,omitempty"`
+}
+
+type EmbedThumbnail struct {
+	Url    string `json:"url"`
+	Height int    `json:"height,omitempty"`
+	Width  int    `json:"widthi,omitempty"`
+}
+
+type EmbedField struct {
+	Name   string `json:"name"`
+	Value  string `json:"value"`
+	Inline bool   `json:"inline,omitempty"`
+}
+
+type Embed struct {
+	Author      EmbedAuthor  `json:"author,omitempty"`
+	Title       string       `json:"title,omitempty"`
+	Url         string       `json:"url,omitempty"`
+	Description string       `json:"description,omitempty"`
+	Timestamp   time.Time    `json:"timestamp,omitempty"`
+	Color       int          `json:"color,omitempty"`
+	Footer      EmbedFooter  `json:"footer,omitempty"`
+	Image       EmbedImage   `json:"image,omitempty"`
+	Thumbnail   EmbedImage   `json:"thumbnail,omitempty"`
+	Fields      []EmbedField `json:"fields,omitempty"`
+}
+
+type SendMessage struct {
+	Content string  `json:"content,omitempty"`
+	Embeds  []Embed `json:"embeds,omitempty"`
+}
