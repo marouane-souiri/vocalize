8146ccd2265502c4a1731f33227d995d1706aed2
Marwane Souiri <marwane.souiri.0@gmail.com>
2025-05-30 21:45:49 +0100

    feat(domain): add missing fields to Member struct

--- internal/domain/member.go
+++ internal/domain/member.go
@@ -1,5 +1,31 @@
 package domain
 
+import "time"
+
+type MemberFlags int
+
+const (
+	MemberFlags_DID_REJOIN MemberFlags = 1 << iota
+	MemberFlags_COMPLETED_ONBOARDING
+	MemberFlags_BYPASSES_VERIFICATION
+	MemberFlags_STARTED_ONBOARDING
+	MemberFlags_IS_GUEST
+	MemberFlags_STARTED_HOME_ACTIONS
+	MemberFlags_COMPLETED_HOME_ACTIONS
+	MemberFlags_AUTOMOD_QUARANTINED_USERNAME
+	MemberFlags_DM_SETTINGS_UPSELL_ACKNOWLEDGED
+)
+
 type Member struct {
-	Permissions *string `json:"permissions"`
+	ID          string      `json:"id"`
+	GuildID     string      `json:"guild_id"`
+	Nickname    string      `json:"nick"`
+	Avatar      string      `json:"avatar"`
+	Banner      string      `json:"banner"`
+	Roles       []string    `json:"roles"`
+	JoinedAt    time.Time   `json:"joined_at"`
+	Flags       MemberFlags `json:"flags"`
+	Deaf        bool        `json:"deaf"`
+	Mute        bool        `json:"mute"`
+	Permissions *string     `json:"permissions"`
 }
